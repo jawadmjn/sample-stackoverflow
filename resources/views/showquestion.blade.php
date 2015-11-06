228b3183391a9a5f3b2e332bfcbb1c3f1dbd707b
Jawad Nawaz <jawadnawaz@live.com>
2015-11-06 15:21:55 +0100

    tabs to spaces

--- resources/views/showquestion.blade.php
+++ resources/views/showquestion.blade.php
@@ -2,31 +2,31 @@
 
 @section('content')
 <div class="container">
-	<div class="row">
-		<div class="col-md-10 col-md-offset-1">
-			<div class="panel panel-default">
-				<div class="panel-heading">Question</div>
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">Question</div>
 
-				<div class="panel-body">
+                <div class="panel-body">
 
-					<h1>Q: {{ $title }}</h1>
+                    <h1>Q: {{ $title }}</h1>
 
-					<?php $i = 1; ?>
-					@foreach ($answer as $key => $value)
-						<h5>Answer {{ $i }}:</h5>
-						<p>
-							{{ $value->description }}
-						</p>
-					<?php $i++; ?>
-					<hr>
-					@endforeach
+                    <?php $i = 1; ?>
+                    @foreach ($answer as $key => $value)
+                        <h5>Answer {{ $i }}:</h5>
+                        <p>
+                            {{ $value->description }}
+                        </p>
+                    <?php $i++; ?>
+                    <hr>
+                    @endforeach
 
-					<h2>Your Answer ?</h2>
-					@include('forms.answerform')
-				</div>
-			</div>
-		</div>
-	</div>
+                    <h2>Your Answer ?</h2>
+                    @include('forms.answerform')
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 @section('javascript')
--- resources/views/tagsearch.blade.php
+++ resources/views/tagsearch.blade.php
@@ -2,46 +2,46 @@
 
 @section('content')
 <div class="container">
-	<div class="row">
-		<div class="col-md-10 col-md-offset-1">
-			<div class="panel panel-default">
-				<div class="panel-heading">Your search for the tag - <b>{{ $tag }}</b></div>
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">Your search for the tag - <b>{{ $tag }}</b></div>
 
-				@include('forms.homeform')
-				<div class="panel-body home_style">
+                @include('forms.homeform')
+                <div class="panel-body home_style">
 
-					@foreach ($questions as $key => $value)
+                    @foreach ($questions as $key => $value)
 
-						<h5>Question:
-							<a href="#" id="home_question" class="btn btn-link js-submit" value="{{ $value->id }}">{{ $value->title }}</a>
-						</h5>
-						<p>
-						<?php
-						// strip tags to avoid breaking any html
-						$string = strip_tags($value->description);
+                        <h5>Question:
+                            <a href="#" id="home_question" class="btn btn-link js-submit" value="{{ $value->id }}">{{ $value->title }}</a>
+                        </h5>
+                        <p>
+                        <?php
+                        // strip tags to avoid breaking any html
+                        $string = strip_tags($value->description);
 
-						if (strlen($string) > 45) {
+                        if (strlen($string) > 45) {
 
-						    // truncate string
-						    $stringCut = substr($string, 0, 45);
+                            // truncate string
+                            $stringCut = substr($string, 0, 45);
 
-						    // make sure it ends in a word so assassinate doesn't become ass...
-						    $string = substr($stringCut, 0, strrpos($stringCut, ' ')).'<span> ....  Click on Question to Read More</span>';
-							echo $string;
-						}
-						else
-						{
-							echo $value->description;
-						}
-						?>
-						</p>
-						<hr>
-					@endforeach
+                            // make sure it ends in a word so assassinate doesn't become ass...
+                            $string = substr($stringCut, 0, strrpos($stringCut, ' ')).'<span> ....  Click on Question to Read More</span>';
+                            echo $string;
+                        }
+                        else
+                        {
+                            echo $value->description;
+                        }
+                        ?>
+                        </p>
+                        <hr>
+                    @endforeach
 
-				</div>
-			</div>
-		</div>
-	</div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @section('javascript')
 <script type="text/javascript">
